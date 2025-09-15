cfd5a4300a3a1a8ffdae7b19ec8487243ab055f2
DaviRdaSilva <davi.rsilva@sptech.school>
2025-09-15 20:30:31 -0300

    Limpar rep

--- scr/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const graficoController = require('../controllers/graficoController');
-
-router.get('/grafico', graficoController.getDadosGrafico);
-
-module.exports = router;
